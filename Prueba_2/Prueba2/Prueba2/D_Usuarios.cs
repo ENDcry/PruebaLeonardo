e29eb5a9f4c91d19599f003e1295fad8043d3839
LEONARDO\PC <leonardo2298@hotmail.com>
2024-07-12 16:18:18 -0600

    Avance de Prueba 2

---
+++ Prueba_2/Prueba2/Prueba2/D_Usuarios.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+
+namespace Prueba2
+{
+    internal class D_Usuarios
+    {
+        public DataTable Listado_cl(string cTexto)
+        {
+            MySqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            MySqlConnection SqlCon = new MySqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                string sql_tarea = "Select Usuarios.userId, Nombres, Paterno, Materno, Sueldo, FechaIngreso " +
+                                   "FROM usuarios "+
+                                   "INNER JOIN empleados ON usuarios.userId = empleados.userId "+
+                                   "where Nombres like '"+cTexto+"' "+
+                                   "ORDER BY usuarios.userId DESC limit 10"
+                ;
+
+
+                MySqlCommand Comando = new MySqlCommand(sql_tarea, SqlCon);
+                Comando.CommandTimeout = 60;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+
+
+        public string Guardar_ar(int nOpcion, P_Usuarios oAr)
+        {
+            string Rpta = "";
+            string Sqltarea = "";
+            //string Sqltarea2 = "";
+            MySqlConnection SqlCon = new MySqlConnection();
+            MySqlConnection SqlCon2 = new MySqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+
+                Console.WriteLine(nOpcion);
+
+                if (nOpcion == 1)  //Nuevo Registro
+                {
+                    Sqltarea = "Insert into usuarios (Nombres, Paterno, Materno )"+
+                                                                        "values('"+oAr.Nombre+"', " +
+                                                                                "'"+oAr.Paterno+"', " +
+                                                                                "'" + oAr.Materno+"'); "+ 
+                                                                                " Insert into empleados (Sueldo, FechaIngreso )"+
+                                                                                "values('" + oAr.sueldo + "', " +
+                                                                                "'" + oAr.FechaIngreso + "');";
+                }
+                else //Actualizar Registro 
+                {
+                   Sqltarea = "update empleados set sueldo ='"+oAr.sueldo+"'" +
+                              "where userId IN (Select userId from usuarios where userId = '"+oAr.userId+"')";
+                }
+                Console.WriteLine(Sqltarea);
+               
+                MySqlCommand Comando = new MySqlCommand(Sqltarea, SqlCon);
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() >= 1 ? "OK" : "No se pudo ingresar el registro";
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+               
+            }
+            return Rpta;
+        }
+    }
+}
---
+++ Prueba_2/Prueba2/Prueba2/frm_usuarios.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba2
+{
+    public partial class frm_usuarios : Form
+    {
+        public frm_usuarios()
+        {
+            InitializeComponent();
+        }
+
+        #region "Mis Variables"
+        int nCodigo_us = 0;
+        int nEstadoGuarda = 0;
+        #endregion
+
+        #region "Mis Metodos"
+
+
+        private void Listado_us(string cTexto)
+        {
+            D_Usuarios Datos = new D_Usuarios();
+            Dgv_usuarios.DataSource = Datos.Listado_cl(cTexto);
+                }
+
+        private void Limpia_texto()
+        {
+            Txt_Name.Text = "";
+            Txt_Paterno.Text = "";
+            Txt_Materno.Text = "";
+            Txt_Sueldo.Text = "";
+            //Txt_Fecha_Ingresa2.Text = "";
+        }
+        private void Seleccion_item() 
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Dgv_usuarios.CurrentRow.Cells["Nombres"].Value)))
+            {
+                MessageBox.Show("Selecciona un registro",
+                                "Aviso de sistema",
+                                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else {
+                this.nCodigo_us = Convert.ToInt32(Dgv_usuarios.CurrentRow.Cells["userId"].Value);
+                Console.WriteLine(nCodigo_us);
+                Txt_Sueldo.Text = Convert.ToString(Dgv_usuarios.CurrentRow.Cells["Sueldo"].Value);
+                Txt_Name.Text = Convert.ToString(Dgv_usuarios.CurrentRow.Cells["Nombres"].Value);
+                Txt_Paterno.Text = Convert.ToString(Dgv_usuarios.CurrentRow.Cells["Paterno"].Value);
+                Txt_Materno.Text = Convert.ToString(Dgv_usuarios.CurrentRow.Cells["Materno"].Value);
+                //Txt_Fecha_Ingresa2.Text = Convert.ToString(Dgv_usuarios.CurrentRow.Cells["FechaIngreso"].Value);
+            }
+        }
+
+        #endregion
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Limpia_texto();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            nEstadoGuarda = 1;
+        }
+
+        private void Dgv_Usuarios_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Btn_Guardar_Click(object sender, EventArgs e)
+        {
+            string Rpta = "";
+            P_Usuarios oAr = new P_Usuarios();
+            oAr.Nombre = Txt_Name.Text;
+            oAr.Paterno = Txt_Paterno.Text;
+            oAr.Materno = Txt_Materno.Text;
+            oAr.sueldo = Convert.ToDouble(Txt_Sueldo.Text);
+            oAr.FechaIngreso = DateTime.Now.ToString("yyyy-MM-dd");
+
+            D_Usuarios Datos = new D_Usuarios();
+            Rpta = Datos.Guardar_ar(nEstadoGuarda, oAr);
+            if (Rpta.Equals("OK"))
+            {
+                nCodigo_us = 0;
+                MessageBox.Show("Los datos se han guardado correctamente",
+                                 "Aviso del sistema",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+            else 
+            {
+          
+                MessageBox.Show(Rpta, "Aviso del sistema",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+
+            
+         }
+        
+
+
+
+        private void frm_usuarios_Load_1(object sender, EventArgs e)
+        {
+            this.Listado_us("%");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_nuevo_Click(object sender, EventArgs e)
+        {
+            nEstadoGuarda = 1;
+            this.Limpia_texto();
+            
+        }
+
+        private void Dgv_usuarios_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            this.Seleccion_item();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
